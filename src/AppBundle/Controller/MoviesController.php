eb2c85f80731ff3bf06ae050760a690c741c9b20
Jacek Wesołowski <jacqu25@yahoo.com>
2018-12-23 12:45:19 +0100

    first CRUD action, validation errors

---
+++ src/AppBundle/Controller/MoviesController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Movie;
+use AppBundle\Exception\ValidationException;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\ControllerTrait;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+class MoviesController extends AbstractController
+{
+    use ControllerTrait;
+
+    /**
+     * @Rest\View()
+     */
+    public function getMoviesAction()
+    {
+        $movies = $this->getDoctrine()->getRepository(Movie::class)->findAll();
+
+        return $movies;
+    }
+
+    /**
+     * @Rest\View(statusCode=201)
+     * @ParamConverter("movie", converter="fos_rest.request_body")
+     * @Rest\NoRoute()
+     */
+    public function postMoviesAction(Movie $movie, ConstraintViolationListInterface $validationErrors)
+    {
+        if (count($validationErrors) > 0) {
+            throw new ValidationException($validationErrors);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($movie);
+        $em->flush();
+
+        return $movie;
+    }
+
+    /**
+     * @Rest\View()
+     */
+    public function deleteMovieAction(?Movie $movie)
+    {
+        if (null === $movie) {
+            return $this->view(null, 404);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($movie);
+        $em->flush();
+    }
+
+    /**
+     * @Rest\View()
+     */
+    public function getMovieAction(?Movie $movie)
+    {
+        if (null === $movie) {
+            return $this->view(null, 404);
+        }
+
+        return $movie;
+    }
+}
